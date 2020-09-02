7be29b6086f6433a1bc3b54f521b08fddc6c90bb
robertvitoriano <robertvitorianodev@gmail.com>
2020-09-02 06:49:46 -0300

    connecting to database

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 const express = require('express');
+const connectDB = require('./config/db');
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+//Connect Database
+
+connectDB();
+
+app.listen(PORT,()=>{
+    
+    console.log('My App is up and running on port: '+PORT);
+})
